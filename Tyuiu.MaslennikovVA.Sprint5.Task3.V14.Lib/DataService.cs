7fdfac05c9970bac493a66f4605a035b74fcfb0f
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-28 00:39:52 +0500

    Task3

---
+++ Tyuiu.MaslennikovVA.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MaslennikovVA.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint5Task3V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+
+            double y = 4 * Math.Pow(x, 3) / (Math.Pow(x, 3) - 1);
+
+            y = Math.Round(y, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
