6e4e16a595ef0ec88acc7668d47d7cabb3061525
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-28 03:39:59 +0500

    Task5

---
+++ Tyuiu.MaslennikovVA.Sprint5.Task5.V29.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MaslennikovVA.Sprint5.Task5.V29.Lib
+{
+    public class DataService : ISprint5Task5V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            int min = 99;
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                int max;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!line.Contains(",")) 
+                    {
+                        max = Convert.ToInt32(line);
+                        if (max % 100 == max && max % 10 != max)
+                        {
+                            if (max < min)
+                                min = max;
+                        }
+                            
+                    }
+                }
+                                                                    //res = res + Convert.ToDouble(line);
+            }
+            return min;
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint5.Task5.V29/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MaslennikovVA.Sprint5.Task5.V29.Lib;
+namespace Tyuiu.MaslennikovVA.Sprint5.Task4.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Масленников В.А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПб-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть набор значений. Найти минимальное целое число   *");
+            Console.WriteLine("* в файле, которое является двузначным числом. Полученный результат       *");
+            Console.WriteLine("* вывести на консоль. У вещественных значений округлить до трёх знаков    *");
+            Console.WriteLine("* после запятой.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask5V0.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Минимальное целое двухзначное число: " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
