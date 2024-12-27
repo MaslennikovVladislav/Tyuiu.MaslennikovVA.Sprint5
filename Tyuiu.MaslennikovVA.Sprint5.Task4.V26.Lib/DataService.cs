392bfb4e30c6c785969e04bdcb2d8c6c2d36f303
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-28 02:51:10 +0500

    Task4

---
+++ Tyuiu.MaslennikovVA.Sprint5.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MaslennikovVA.Sprint5.Task4.V26.Lib
+
+{
+    public class DataService : ISprint5Task4V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path).Replace(".",",");
+            double res = Math.Round((Math.Pow(Convert.ToDouble(strX), 3) + Math.Sin(Convert.ToDouble(strX))) * Math.Cos(Convert.ToDouble(strX)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint5.Task4.V26/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+using Tyuiu.MaslennikovVA.Sprint5.Task4.V26.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint5.Task4.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Масленников В.А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПб-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть вещественное значение. Прочитать значение из    *");
+            Console.WriteLine("* файла и подставить вместо Х в формуле y = (x^3+sin(x))cos(x) вычислить  *");
+            Console.WriteLine("* значение по формуле (Полученное значение округлить до трёх знаков после *");
+            Console.WriteLine("* запятой) и вернуть полученный результат на консоль.                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V0.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
