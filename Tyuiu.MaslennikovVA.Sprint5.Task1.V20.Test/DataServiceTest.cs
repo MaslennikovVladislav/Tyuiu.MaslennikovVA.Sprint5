f57e0c6a1f96e8fe36e07597a0092eab4699377d
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-28 00:17:17 +0500

    Task1

---
+++ Tyuiu.MaslennikovVA.Sprint5.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint5.Task1.V20.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint5.Task1.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\vladm\source\reposit\Tyuiu.MaslennikovVA.Sprint5\Tyuiu.MaslennikovVA.Sprint5.Task1.V20\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
