2d916a298683e1a00cba343978a6784280efd45c
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-28 05:00:26 +0500

    Task7

--- Tyuiu.MaslennikovVA.Sprint5.Task7.V12.Lib/DataService.cs
+++ Tyuiu.MaslennikovVA.Sprint5.Task7.V12.Lib/DataService.cs
@@ -6,12 +6,12 @@ namespace Tyuiu.MaslennikovVA.Sprint5.Task7.V12.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            int count = 0;
+
             StreamReader reader = new StreamReader(path + @"\InPutDataFileTask7V0.txt");
             string line = reader.ReadLine().ToUpper();
             FileInfo fileInfo = new FileInfo(path + @"\OutPutDataFileTask7V0.txt");
-            if  (!fileInfo.Exists) 
-                File.Create(path + @"\OutPutDataFileTask7V0.txt" );
+            //if (!fileInfo.Exists)
+            //    File.Create(path + @"\OutPutFileTask7V0.txt");
 
             StreamWriter writer = new StreamWriter(path + @"\OutPutDataFileTask7V0.txt");
             writer.WriteLine(line);
